afcd12883a713823e20c8ad3571a1d7843bd8238
henryxiao1997 <xiaojinghui1978@qq.com>
2021-02-25 23:24:01 +0800

    remove non-tensor in forward function so as to support tracing

--- RNNLM.py
+++ RNNLM.py
@@ -44,32 +44,33 @@ class RNNConfig(object):
 class RNNLM(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.dropout = nn.Dropout(1 - config.dp_keep_prob)
-        self.word_embeddings = nn.Embedding(config.vocab_size, config.embedding_dim)
-        if config.type == 'lstm':
+        self.config = config
+        self.dropout = nn.Dropout(1 - self.config.dp_keep_prob)
+        self.word_embeddings = nn.Embedding(self.config.vocab_size, self.config.embedding_dim)
+        if self.config.type == 'lstm':
             self.rnn = nn.LSTM(
-                    input_size = config.embedding_dim,
-                    hidden_size = config.embedding_dim,
-                    num_layers = config.num_layers,
-                    bidirectional = config.bidirectional,
-                    dropout = 1 - config.dp_keep_prob)
-        elif config.type == 'gru':
+                    input_size = self.config.embedding_dim,
+                    hidden_size = self.config.embedding_dim,
+                    num_layers = self.config.num_layers,
+                    bidirectional = self.config.bidirectional,
+                    dropout = 1 - self.config.dp_keep_prob)
+        elif self.config.type == 'gru':
             self.rnn = nn.GRU(
-                    input_size = config.embedding_dim,
-                    hidden_size = config.embedding_dim,
-                    num_layers = config.num_layers,
-                    bidirectional = config.bidirectional,
-                    dropout = 1 - config.dp_keep_prob)
+                    input_size = self.config.embedding_dim,
+                    hidden_size = self.config.embedding_dim,
+                    num_layers = self.config.num_layers,
+                    bidirectional = self.config.bidirectional,
+                    dropout = 1 - self.config.dp_keep_prob)
         else:
             print('not support model type, please input lstm or gru')
-        if config.bidirectional:
+        if self.config.bidirectional:
             self.sm_fc = nn.Linear(
-                    in_features = config.embedding_dim * 2,
-                    out_features = config.vocab_size)
+                    in_features = self.config.embedding_dim * 2,
+                    out_features = self.config.vocab_size)
         else:
             self.sm_fc = nn.Linear(
-                    in_features = config.embedding_dim,
-                    out_features = config.vocab_size)
+                    in_features = self.config.embedding_dim,
+                    out_features = self.config.vocab_size)
         self.init_weights()
 
     def init_weights(self):
@@ -78,32 +79,32 @@ class RNNLM(nn.Module):
         self.sm_fc.bias.data.fill_(0.0)
         self.sm_fc.weight.data.uniform_(-init_range, init_range)
 
-    def init_hidden(self, config, batch_size):
+    def init_hidden(self, batch_size):
         weight = next(self.parameters()).data
-        if config.type == 'lstm':
-            if config.bidirectional:
-                return (Variable(weight.new(config.num_layers*2, batch_size, config.embedding_dim).zero_()),
-                        Variable(weight.new(config.num_layers*2, batch_size, config.embedding_dim).zero_()))
+        if self.config.type == 'lstm':
+            if self.config.bidirectional:
+                return (Variable(weight.new(self.config.num_layers*2, batch_size, self.config.embedding_dim).zero_()),
+                        Variable(weight.new(self.config.num_layers*2, batch_size, self.config.embedding_dim).zero_()))
             else:
-                return (Variable(weight.new(config.num_layers, batch_size, config.embedding_dim).zero_()),
-                        Variable(weight.new(config.num_layers, batch_size, config.embedding_dim).zero_()))
-        elif config.type == 'gru':
-            if config.bidirectional:
-                return Variable(weight.new(config.num_layers*2, batch_size, config.embedding_dim).zero_())
+                return (Variable(weight.new(self.config.num_layers, batch_size, self.config.embedding_dim).zero_()),
+                        Variable(weight.new(self.config.num_layers, batch_size, self.config.embedding_dim).zero_()))
+        elif self.config.type == 'gru':
+            if self.config.bidirectional:
+                return Variable(weight.new(self.config.num_layers*2, batch_size, self.config.embedding_dim).zero_())
             else:
-                return Variable(weight.new(config.num_layers, batch_size, config.embedding_dim).zero_())
+                return Variable(weight.new(self.config.num_layers, batch_size, self.config.embedding_dim).zero_())
         else:
             print('not support model type, please input lstm or gru')
 
-    def forward(self, config, inputs, hidden):
+    def forward(self, inputs, hidden):
         embeds = self.dropout(self.word_embeddings(inputs))
         rnn_out, hidden = self.rnn(embeds, hidden)
         rnn_out = self.dropout(rnn_out)
-        if config.bidirectional:
-            logits = self.sm_fc(rnn_out.view(-1, config.embedding_dim*2))
+        if self.config.bidirectional:
+            logits = self.sm_fc(rnn_out.view(-1, self.config.embedding_dim*2))
         else:
-            logits = self.sm_fc(rnn_out.view(-1, config.embedding_dim))
-        return logits.view(inputs.shape[0], inputs.shape[1], config.vocab_size), hidden
+            logits = self.sm_fc(rnn_out.view(-1, self.config.embedding_dim))
+        return logits.view(inputs.shape[0], inputs.shape[1], self.config.vocab_size), hidden
         #return logits.view(self.num_steps, self.batch_size, self.vocab_size), hidden
 
 def main():
